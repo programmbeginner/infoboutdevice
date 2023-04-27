fc414c84f3fa79a5e7307815aa235f490d0d7288
programmbeginner <104715102+programmbeginner@users.noreply.github.com>
2023-04-27 17:13:50 +0200

    Add files via upload

---
+++ InfoPlatformView.py
@@ -0,0 +1,10 @@
+import platform
+import numbers
+
+input("Type 1 for Info!: ")
+
+if == "1";
+
+print("You are currently using:", platform.system(), platform.version(), platform.node(), platform.release())
+
+return 0();
